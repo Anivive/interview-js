e88421a7f059242cfc8c4188f7e7e83df526f594
Nick Sirago <nick@sirago.org>
2020-10-23 14:13:21 -0700

    npm update

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Anivive"/>
   </div>
 </template>
 
@@ -12,7 +12,12 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  methods: () => ({
+    created() {
+      console.log("test")
+    }
+  })
 }
 </script>
 
